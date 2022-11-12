7defdb39c4c91ea3034f47522b2bca66b6314a8b
Norbert Borek <norbertborek92@gmail.com>
2022-11-12 13:45:33 +0100

    done

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.loops.task3;
 
 public class AlphabetPrinter {
     public void printAlphabet() {
+        for (char i = 'a'; i <= 'z'; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        AlphabetPrinter alphabetPrinter = new AlphabetPrinter();
+        alphabetPrinter.printAlphabet();
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        int num1 = 0, num2 = 1;
+        for (int i = 1; i <= lastFibonacci; ++i)
+        {
+            System.out.println(num1);
+            /* On each iteration, we are assigning second number
+             * to the first number and assigning the sum of last two
+             * numbers to the second number
+             */
+            int sumOfPrevTwo = num1 + num2;
+            num1 = num2;
+            num2 = sumOfPrevTwo;
+        }
     }
 }
